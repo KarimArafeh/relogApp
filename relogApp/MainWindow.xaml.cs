b85ce5f5a31806a4a3a2ca177b8a233931a67634
y2793623b <y2793623b@aula73-42>
2017-03-13 19:36:48 +0100

    acabar

--- relogApp/MainWindow.xaml.cs
+++ relogApp/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
+
+
 namespace relogApp
 {
     /// <summary>
@@ -26,16 +28,20 @@ namespace relogApp
         private DispatcherTimer dispatcherTimer;
         private DateTimeOffset lastTime;
         private DateTimeOffset startTime;
+        private String Alarm;
+        private String AlarmaAntigua;
 
         public MainWindow()
         {
-
             InitializeComponent();
+            Initialize();
             DispatcherTimerSetup();
 
-            txt_data.Text = "Time : ";
+        }
 
-
+        public void Initialize()
+        {
+            
         }
 
         public void DispatcherTimerSetup()
@@ -43,25 +49,84 @@ namespace relogApp
             dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += dispatcherTimer_Tick;
             dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            //IsEnabled defaults to false
-            //TimerLog.Text += "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
+            
             startTime = DateTimeOffset.Now;
             lastTime = startTime;
-            //TimerLog.Text += "Calling dispatcherTimer.Start()\n";
+            
             dispatcherTimer.Start();
-            //IsEnabled should now be true after calling start
-            //TimerLog.Text += "dispatcherTimer.IsEnabled = " + dispatcherTimer.IsEnabled + "\n";
+            
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            txt_data.Text = DateTime.Now.ToString();
+            String[] date = DateTime.Now.ToString().Split(' ');
+            txt_data.Text = date[1];
+            date = txt_data.Text.Split(':');
+            
+            if (cbAlarmaOn.IsChecked == true && Alarm != null)
+            {
+                if (Alarm.Equals(date[0] + ":" + date[1]))
+                {
+                    System.Media.SystemSounds.Asterisk.Play();
+                    MessageBox.Show("es el momento");
+                    Alarm = null;
+                }
+            }
+            if (cbAlarmaOn.IsChecked == true && AlarmaAntigua != null && AlarmaAntigua.Equals(date[0] + ":" + date[1]))
+            {
+                System.Media.SystemSounds.Beep.Play();
+                MessageBox.Show("es el momento");
+                AlarmaAntigua = null;
+            }
+                
+        }
+
+        private void Sobre_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" by : KARIM ARAFEH");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cbAlarmaOn_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbAlarmaOn.IsChecked == true)
+            {
+                String text = System.IO.File.ReadAllText("../../Alarma.txt");
+                MessageBox.Show("Tienes alarma guardad a les : " + text + " \n si quieres modificar la \n escribe la nueva alarma y guarda \n y si no te avisamos cuando sea el momento");
+                AlarmaAntigua = text;
+                lblAlarmHora.Visibility = Visibility.Visible;
+                txtAlarmHora.Visibility = Visibility.Visible;
+                btSave.Visibility = Visibility.Visible;
+            }
+            else if (cbAlarmaOn.IsChecked == false)
+            {
+                lblAlarmHora.Visibility = Visibility.Hidden;
+                txtAlarmHora.Clear();
+                txtAlarmHora.Visibility = Visibility.Hidden;
+                btSave.Visibility = Visibility.Hidden;
+            }
+                    
+        }
+
+        private void btSave_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Alarm = txtAlarmHora.Text;
+            if (Alarm != "")
+            {
+                MessageBox.Show("la alarma esta guardad \n cuando sea el momento te avisamos .");
+                System.IO.File.WriteAllText("../../Alarma.txt", Alarm);
+                txtAlarmHora.Text = null;
+            }
+            else MessageBox.Show("No introduciste tiempo por la alarma !!!!!");
             
         }
 
-        private void txt_data_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
+
+
 }
